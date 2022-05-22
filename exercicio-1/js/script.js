fcf2db1d7d1b86ba8425c38facf9063a397b4e0b
Uziel Menezes <uzielcivil@gmail.com>
2022-05-22 15:44:51 -0300

    more tests in js

--- exercicio-1/js/script.js
+++ exercicio-1/js/script.js
@@ -1,13 +1,20 @@
 let carro = {
     portas: 4,
-    cor: "Vermelha"
+    cor: "Vermelha",
+    adicionais: [
+        "ar-condicionado",
+        "som",
+        "teto solar"
+    ],
+    vaiEquipado: false
 }
-
-console.log(carro);
-
 
 let pessoa = {
     nome: "Uziel"
 }
 
-console.log(pessoa);
+console.log(carro.adicionais);
+console.log(carro.adicionais[0].length);
+if(carro.vaiEquipado == false) {
+    console.log("não vai equipado");
+}
