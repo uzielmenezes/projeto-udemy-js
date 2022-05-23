c9a77ee4536e6a1dff6c419ea6711232b38238f3
Uziel Menezes <uzielcivil@gmail.com>
2022-05-23 20:34:25 -0300

    copying folder

---
+++ exercicio_2/js/script.js
@@ -0,0 +1,18 @@
+/*let carro = {
+    nome: "BMW", // string
+    portas: 4, // number
+    cor: "Vermelha", // string
+    adicionais: [
+        "ar-condicionado",  // array
+        "som",
+        "teto solar"
+    ],
+    vaiEquipado: false, // boolean
+    acelerar() {   // function
+        console.log("vrawnnnn");
+    },
+    getNome() {
+        
+    }
+}
+carro.acelerar(); */
