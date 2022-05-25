da6fdc8aa421b63d059cd1c1413eeecf85f04b13
Uziel Menezes <uzielcivil@gmail.com>
2022-05-25 13:19:51 -0300

    exemplo_array

---
+++ exemplo_array/script.js
@@ -0,0 +1,8 @@
+let a = new Array(1,2,3,4,5);
+a[5] = 6;
+let b = [1, 2 , 3];
+
+console.log(a, b);
+console.log(a instanceof Array);
+console.log(a instanceof Object);
+console.log(b instanceof Object);
