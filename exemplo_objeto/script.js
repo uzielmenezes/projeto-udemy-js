0cd26d4c0aafa2d893e5a7c6ea06b7df895b1c37
Uziel Menezes <uzielcivil@gmail.com>
2022-05-25 13:14:19 -0300

    exemplo_objeto

---
+++ exemplo_objeto/script.js
@@ -0,0 +1,18 @@
+let o1 = {};
+let o2 = new Object();
+
+console.log(o1);
+console.log(o2);
+
+o1.nome = 'Uziel';
+o1.sobrenome = 'Menezes';
+
+o2.nome = 'Caio';
+o2.sobrenome = 'Falcão';
+
+console.log(o1.valueOf());
+console.log(o2.valueOf());
+
+                // Object
+console.log(o1.toString()); 
+console.log(o2.toString()); 
